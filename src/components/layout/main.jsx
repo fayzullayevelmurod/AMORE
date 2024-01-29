a3ecfff298548500423882eade19f556351724e8
QobilovSardor <note47297@gmail.com>
2024-01-29 12:05:45 +0500

    amore and get-valintine section media query finished

--- src/components/layout/main.jsx
+++ src/components/layout/main.jsx
@@ -9,13 +9,13 @@ export const Main = () => {
 	return (
 		<main className="main">
 			<Intro />
-			{/* <GetValentine /> */}
-			{/* <div className="container">
+			<GetValentine />
+			<div className="container">
 				<Amore />
-				<Rodmap />
+				{/* <Rodmap /> */}
 			</div>
-			<Tokenomics />
-			<Faq /> */}
+			{/* <Tokenomics /> */}
+			{/* <Faq /> */}
 		</main>
 	)
 }
